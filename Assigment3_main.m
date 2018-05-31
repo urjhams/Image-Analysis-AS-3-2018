58e864554b77c8d51c738b95cd709036f41ac4d7
luigielbambino <luis_al82@hotmail.com>
2018-05-31 13:48:51 +0200

    Add c part
    Make minor changes in the code and add c part after testing with different methods and values.

--- Assigment3_main.m
+++ Assigment3_main.m
@@ -1,12 +1,43 @@
-sigma = 1;
-originalImage = imread('input_ex3.jpg');
-figure; subplot(1,1,1), imshow(originalImage); title('original');
+function Assignment3_main()
+  pkg load image;
+  
+  sigma = 1;
+  originalImage = imread('input_ex3.jpg');
+  
 
-%a
-grayScale = mat2gray(originalImage);
-figure; plot(1,1,1), imshow(grayScale,[]); title('gray scale image');
+  %a Grayscale
+  grayScale = mat2gray(originalImage);
+ 
 
-%b
-[Ix, Iy] = Gradient(mean(grayScale,3),sigma);
-mag = sqrt(Ix.^2 + Iy.^2);
-figure; plot(1,1,1), imshow(mag,[]); title('magnitude');
+  %b Gradient magnitude
+  [Ix, Iy] = Gradient(mean(grayScale,3),sigma);
+  mag = sqrt(Ix.^2 + Iy.^2);
+  
+  %c Binary mask. Tried to use imbinarize but could not test that function in octave
+  %binary = im2bw (mag, "mean");
+  %binary = im2bw (mag, "concavity");
+  binary = im2bw (mag, 0.07);
+  
+  
+  %d Hough transform
+  
+  
+  %e Voting array
+  
+  %f Local maxima - Hough peaks
+  %peaks  = houghpeaks(H,2);
+  
+  %g Found extrema
+  
+  %h Hough lines
+  
+  %i Final overlay
+  
+  
+  
+  subplot(2,2,1), imshow(originalImage); title('Original');
+  subplot(2,2,2), imshow(grayScale,[]); title('a) gray scale');
+  subplot(2,2,3), imshow(mag,[]); title('b) Gradient magnitude');
+  subplot(2,2,4), imshow(binary); title('c) Binary mask');
+  
+End
--- Gradient.m
+++ Gradient.m
@@ -1,5 +1,4 @@
 function [Ix, Iy] = Gradient(I, sigma)
-% =============================
   r = round(3*sigma);
   i = -r:r;
   g = exp(-i.^2 / (2*sigma^2)) / (sqrt(2*pi)*sigma); d = -i.*g / sigma^2;
