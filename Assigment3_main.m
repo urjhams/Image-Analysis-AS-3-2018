55f19deed02050eef8de75aa077b62d63f6949c4
Quan <urjhams@gmail.com>
2018-06-05 22:02:24 +0200

    part d to h
    got the problem with octave, should check agains the fuction in new file - which include task d.
    still have final part i left

--- Assigment3_main.m
+++ Assigment3_main.m
@@ -1,43 +1,47 @@
 function Assignment3_main()
   pkg load image;
-  
+
   sigma = 1;
   originalImage = imread('input_ex3.jpg');
-  
 
-  %a Grayscale
+
+  %------------------ a Grayscale
   grayScale = mat2gray(originalImage);
- 
 
-  %b Gradient magnitude
+  %------------------ b Gradient magnitude
   [Ix, Iy] = Gradient(mean(grayScale,3),sigma);
   mag = sqrt(Ix.^2 + Iy.^2);
-  
-  %c Binary mask. Tried to use imbinarize but could not test that function in octave
+
+  %------------------ c Binary mask.
+  %Tried to use imbinarize but could not test that function in octave
   %binary = im2bw (mag, "mean");
   %binary = im2bw (mag, "concavity");
   binary = im2bw (mag, 0.07);
-  
-  
-  %d Hough transform
-  
-  
-  %e Voting array
-  
-  %f Local maxima - Hough peaks
-  %peaks  = houghpeaks(H,2);
-  
-  %g Found extrema
-  
-  %h Hough lines
-  
-  %i Final overlay
-  
-  
-  
+
+  %------------------ d Hough transform implement
+  [H,thetas,P] = hough_Detection(bw,mag);
+
+  %------------------ e Voting array ploting
+  figure; plot(1, 1, 1), imagesc(thetas, P, H), colormap('gray'); title('Hough voting result');
+
+  %------------------ f Local maxima - Hough peaks
+  peaks = houghpeaks(H, 20, 'threshold', ceil(0.3 * max(H(:))));
+
+  %------------------ g Found extrema
+  figure; subplot(3, 2, 4), imagesc(thetas, P, H), colormap('gray'); title('Hough peaks');
+  axis on, axis normal, hold on;
+  plot(thetas(P(:, 2)), P(peaks(:, 1)), 's', 'color', 'blue');
+
+  %------------------ h Hough lines
+  lines = houghlines(bw, thetas, P, peaks,'FillGap', 20, 'MinLength', 20);
+
+  %------------------ i Final overlay
+
+
+  figure;
   subplot(2,2,1), imshow(originalImage); title('Original');
   subplot(2,2,2), imshow(grayScale,[]); title('a) gray scale');
   subplot(2,2,3), imshow(mag,[]); title('b) Gradient magnitude');
   subplot(2,2,4), imshow(binary); title('c) Binary mask');
-  
+
 End
---
+++ hough_Detection.m
@@ -0,0 +1,24 @@
+function [Hough,thetaRange,pRange] = hough_Detection(bw,gradient)
+  [rows, cols] = size(bw);
+  thetaMax = 90;
+  pMax = ceil(sqrt((w - 1).^2 + (h - 1).^2));   % ceil in octave = floor in matlab
+  thetaRange = -90:(thetaMax - 1);
+  pRange = -pMax:pMax;
+  
+  Hough = zeros(pMax * 2 + 1,180);
+  
+  for row = 1:rows
+    for col = 1:cols
+      if bw(row,col) > 0
+        x = col - 1;
+        y = row - 1;
+        for theta = thetaRange
+          p = round(x * cosd(theta) + y * sind(theta));
+          pIndex = p + pMax + 1;
+          thetaIndex = theta + thetaMax + 1;
+          Hough(pIndex, thetaIndex) = Hough(pIndex, thetaIndex) + 1
+        end
+      end
+    end
+  end
+end
