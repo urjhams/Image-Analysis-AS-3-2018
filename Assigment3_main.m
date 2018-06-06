699bad148c348d738556ceeb8b558ce319924f32
Quan <urjhams@gmail.com>
2018-06-06 17:33:58 +0200

    done the d part
    Seem like just syntax error (honestly i still don't know which one :( - Quan)
    at least it run correctly :D (on Matlab!)

--- Assigment3_main.m
+++ Assigment3_main.m
@@ -41,7 +41,8 @@ function Assignment3_main()
   for k = 1:length(lines)
     xy = [lines(k).point1; lines(k).point2];
     plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-    % Plot beginnings and ends of lines
+
+    % --- ploting begin
     plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
     plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
     len = norm(lines(k).point1 - lines(k).point2);
--- hough_Detection.m
+++ hough_Detection.m
@@ -1,17 +1,16 @@
-function [H,theta_range,p_range] = hough_Detection(bw)
+function [H, theta_range, rho] = hough_Detection(bw)
   [w, h] = size(bw);
-  pMax = ceil(sqrt(w.^2 + h.^2));
   theta_range = -90:89;
-  p_range = -pMax:pMax;
-  H = zeros(2 * pMax + 1, length(theta_range));
-
-  for x = 1:w
-    for y = 1:h
-      if bw(x,y) ~= 0
-        for thetaIndex = 1:length(theta_range)
-          p = round(x * cos(theta_range(thetaIndex) + pi/180) + y * sin(theta_range(thetaIndex) + pi/180));
-          pIndex = round(p + pMax) + 1;
-          H(pIndex,thetaIndex) = H(pIndex,thetaIndex) + 1;
+  dMax = ceil(sqrt(w ^ 2 + h ^ 2));
+  rho = -dMax : dMax;
+  H = zeros(2 * dMax, length(theta_range));
+  for x = 1 : w
+    for y = 1 : h
+      if (bw(x, y))
+        for thetaIndex = 1 : length(theta_range)
+          p = y * cos(theta_range(thetaIndex) * pi / 180) + x * sin(theta_range(thetaIndex) * pi / 180);
+          pIndex = round(p + dMax) + 1;
+          H(pIndex, thetaIndex) = H(pIndex, thetaIndex) + 1;
         end
       end
     end
