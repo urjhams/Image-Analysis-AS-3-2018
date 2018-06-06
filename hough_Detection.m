8e4a6897326602163c9222cf20bab6a8bf67bc97
Quan <urjhams@gmail.com>
2018-06-06 15:30:57 +0200

    d part correction

--- hough_Detection.m
+++ hough_Detection.m
@@ -1,9 +1,9 @@
 function [H,theta_range,p_range] = hough_Detection(bw)
-  [h, w] = size(bw);
+  [w, h] = size(bw);
   pMax = ceil(sqrt(w.^2 + h.^2));
   theta_range = -90:89;
   p_range = -pMax:pMax;
-  H = zeros(2 * pMax + 1, length(p_range));
+  H = zeros(2 * pMax + 1, length(theta_range));
 
   for x = 1:w
     for y = 1:h
